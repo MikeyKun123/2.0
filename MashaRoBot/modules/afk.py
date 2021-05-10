cbfbdbaeeaf16b25b465eae8963917692c9f6597
Rs-45 <83909902+Rs-45@users.noreply.github.com>
2021-05-10 18:28:54 +0530

    Updated

--- MashaRoBot/modules/afk.py
+++ MashaRoBot/modules/afk.py
@@ -38,7 +38,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(fname, notice))
+        update.effective_message.reply_text("User {} is now away! Let's Not Disturb them {}".format(fname, notice))
     except BadRequest:
         pass
 
@@ -58,14 +58,17 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
+                "Summoning Jutsu...{} is here Now!",
+                "Fear Not Citizens {} is back Online!",
+                "Hey Sakura it's Not Our Sasuke...it's Just {} Back Online!",
+                "OwO Finally {} is awake!",
+                "UwU Welcome Back {} Now Give Me Some Ramen!",
+                "{} is finally Out Of Itachi's Genjutsu and back here!",
+                "Calm Down KURAMA...it's Just {} Back In the Chat!",
                 "Where is {}?\nIn the chat!",
+                "Our Ninja {} Has Completed The Mission and Now is Back In the Chat!",
+                "Hey {} Welcome Back but always Remember life's greatest lessons are learned through pain!",
+                "Ladies and Gentlemen {} is Back In the Chat! Let's Ask Him for Some Treat",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
@@ -129,10 +132,10 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         if int(userc_id) == int(user_id):
             return
         if not user.reason:
-            res = "{} is afk".format(fst_name)
+            res = "This Ninja {} is afk Probably finding Our Sasuke".format(fst_name)
             update.effective_message.reply_text(res)
         else:
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "This Ninja {} is afk.\nReason: <code>{}</code>".format(
                 html.escape(fst_name), html.escape(user.reason)
             )
             update.effective_message.reply_text(res, parse_mode="html")
