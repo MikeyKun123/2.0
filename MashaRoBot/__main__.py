eee32938cbeccbb4d851abc7d309fc897e31b7af
Rs-45 <83909902+Rs-45@users.noreply.github.com>
2021-05-11 21:10:31 +0530

    updated img

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -693,7 +693,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Lord Hokage,I'M Back From My Mission...Now Give Me Some Ramen! :)")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Well I'm Back...Now Give Me Some Ramen! :)")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
