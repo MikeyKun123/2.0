6556bcc6747f4bdb4d28faa925e8b40f08db23dd
MikeyKun123 <83909902+MikeyKun123@users.noreply.github.com>
2021-07-04 21:37:39 +0530

    tp

--- MashaRoBot/modules/warns.py
+++ MashaRoBot/modules/warns.py
@@ -172,7 +172,7 @@ def button(update: Update, context: CallbackContext) -> str:
         res = sql.remove_warn(user_id, chat.id)
         if res:
             update.effective_message.edit_text(
-                "Warn removed by {}.".format(mention_html(user.id, user.first_name)),
+                "Damn Lucky Fella Your Warn removed by {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
             user_member = chat.get_member(user_id)
